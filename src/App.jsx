e851afd5ac967b85ae038845b8ca41483bff63af
Achel-Sahe <achelsahetapy10@gmail.com>
2026-03-09 15:07:21 +0700

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/home";
 import Articles from "./pages/articles";
 import Products from "./pages/products";
 import About from "./pages/about";
 import ContactPage from "./pages/contactPage";
+
 function App() {
   return (
-    <Router>
+    <Router basename="/skincare-website-nengnanum-beauty/">
       <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
-      <Routes>
         <Route path="/about" element={<About />} />
-      </Routes>
-      <Routes>
         <Route path="/products" element={<Products />} />
-      </Routes>
-      <Routes>
-        <Route path="/articles" element={<Articles/>} />
-      </Routes>
-      <Routes>
-        <Route path="/contact" element={<ContactPage/>} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Router>
   );
